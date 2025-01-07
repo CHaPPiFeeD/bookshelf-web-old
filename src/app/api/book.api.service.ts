cd05fcde106fc663c185c2d9d45d1cec74f1e6f6
Dmitry Shevchuk <sheva2dim@gmail.com>
2025-01-07 14:49:25 +0200

    show books in profile

--- src/app/api/book.api.service.ts
+++ src/app/api/book.api.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from "@angular/core";
 import { environment } from "../../environments/environment";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { map, Observable } from "rxjs";
+import { BookUserStatus } from "../components/book-card-list/book-card-list.enum";
 
 @Injectable()
 export class BookApiService {
   private apiRoute: string = `${environment.apiUrl}/api/books`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   createBook(payload: CreateBookPayload): Observable<Object> {
     return this.http.post(this.apiRoute, payload);
   }
+
+  getAllBooks(params: GetAllBooksQuery) {
+    return this.http.get(this.apiRoute, { params }).pipe(map((res: any) => res.data));
+  }
+}
+
+export type GetAllBooksQuery = {
+  user_id?: string;
+  book_user_status?: BookUserStatus;
 }
 
 type CreateBookPayload = {
--- src/app/components/book-card-list/book-card-list.component.ts
+++ src/app/components/book-card-list/book-card-list.component.ts
@@ -1,19 +1,40 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { BookUserStatus } from "./book-card-list.enum";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
+import { BookApiService, GetAllBooksQuery } from "../../api/book.api.service";
 
 @Component({
   selector: 'app-book-card-list',
   templateUrl: 'book-card-list.component.html',
   styleUrls: ['book-card-list.component.scss']
 })
-export class BookCardListComponent {
+export class BookCardListComponent implements OnInit {
   @Input() bookMark!: BookUserStatus;
-  BookUserStatus = BookUserStatus
+  books!: any;
+  BookUserStatus = BookUserStatus;
 
   constructor(
+    private bookApiService: BookApiService,
+    private activatedRoute: ActivatedRoute,
     private router: Router,
-  ) {}
+  ) { 
+   
+  }
+
+  ngOnInit(): void {
+    this.loadBookList();
+  }
+
+  loadBookList() {
+    const params: GetAllBooksQuery = {
+      book_user_status: this.bookMark,
+      user_id: this.activatedRoute.snapshot.params['id'],
+    };
+    this.bookApiService.getAllBooks(params).subscribe({
+      next: (data) => { this.books = data },
+      error: (err) => { console.error(err) },
+    });
+  }
 
   createBook() {
     this.router.navigate(['book', 'create'])
